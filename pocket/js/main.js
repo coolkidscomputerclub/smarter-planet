93bcff068bcefaf61e1c0ed7e1928a09a90a9000
Ben Ashman <ben@benashman.co.uk>
2012-12-13 14:47:07 +0000

    Successfully won my battle with Handlebars

--- pocket/js/main.js
+++ pocket/js/main.js
@@ -1,26 +1,25 @@
 $(function(){
 
-	var index = 0;
-
-	// Make first slice active
-	$(".user-select li:first").addClass('active');
-
-	// User select slide event
-	$(".user-select").bind('slide', function() {
-
-		// Remove active class
-		$(".user-select li.active").removeClass('active');
-
-		if (index < 3) {
-			index++;
-		} else {
-			index--;
+	// Set up user-select slider
+	window.userSlider = new Swipe(document.getElementById('user-select'), {
+		callback: function() {
+			console.log("Current user ID: ", this.getPos());
 		}
-
-		$(".user-select li:eq(" +  index + ")").addClass('active');
-
-		console.log("User changed", index);
-
 	});
+
+	var userData = {
+		users: [
+			{ id: 0, name: "Ben", 	avatarURL: "img/avatar/ben.jpg" 	},
+			{ id: 1, name: "James", avatarURL: "img/avatar/james.jpg" 	},
+			{ id: 2, name: "Flo", 	avatarURL: "img/avatar/flo.jpg" 	},
+			{ id: 3, name: "Saul", 	avatarURL: "img/avatar/saul.jpg" 	}
+		]
+	};
+
+	// Family member templating
+	var familySource = $("#family-template").html();
+	var familyTemplate = Handlebars.compile(familySource);
+
+	$("#family").append(familyTemplate(userData));
 
 });
