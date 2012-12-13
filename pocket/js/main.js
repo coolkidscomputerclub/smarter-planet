374dd40921641ebf9b71db731cf22bfdf7cf834b
Ben Ashman <ben@benashman.co.uk>
2012-12-13 19:57:35 +0000

    Added mobile interface with presence status and messages

--- pocket/js/main.js
+++ pocket/js/main.js
@@ -34,7 +34,6 @@ var main = {
         this.socket.on("presence", function (data) {
 
             console.log("Presence: ", data.user.presence);
-            console.log("Timestamp", data);
 
             // Compile Handebars timeline template
 			var timelineSource = $("#presence-event-template").html();
@@ -49,8 +48,17 @@ var main = {
 
         	console.log("Users", data);
 
+        	// Compile Handlebars user selector template
+			var familySource = $("#family-template").html();
+			var familyTemplate = Handlebars.compile(familySource);
+
+			$("#user-select").html(familyTemplate(data));
+
+			window.userSelector = new Swipe(document.getElementById("user-select"));
 
         });
+
+
 
     }
 
@@ -60,41 +68,5 @@ $(function(){
 
 	// Initialise and run sockets
 	main.init();
-
-	// Test data
-	var userData = {
-		users: [
-			{ id: 0, name: "Ben", 	avatarURL: "img/avatar/ben.jpg" 	},
-			{ id: 1, name: "James", avatarURL: "img/avatar/james.jpg" 	},
-			{ id: 2, name: "Flo", 	avatarURL: "img/avatar/flo.jpg" 	},
-			{ id: 3, name: "Saul", 	avatarURL: "img/avatar/saul.jpg" 	}
-		]
-	};
-
-	// HB: User Selection
-	var familySource = $("#family-template").html();
-	var familyTemplate = Handlebars.compile(familySource);
-
-	$("#user-select").append(familyTemplate(userData));
-
-	// Set up user selection
-	var userSlider = new Swipe(document.getElementById('user-select'), {
-
-		callback: function() {
-
-			var activeUser = "Ben";
-
-			switch(this.getPos()) {
-
-				case 0: activeUser = "Ben"; 	break;
-				case 1: activeUser = "James"; 	break;
-				case 2: activeUser = "Flo"; 	break;
-				case 3: activeUser = "Saul"; 	break;
-
-			}
-
-			console.log("Current User:", activeUser);
-		}
-	});
 
 });
